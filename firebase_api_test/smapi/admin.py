d763952f8df44d95e004d42520ded3c15fdaf8a8
kjb4494 <rlagnltn1357@gmail.com>
2019-05-17 16:42:45 +0900

    Add admin db viewer

--- firebase_api_test/smapi/admin.py
+++ firebase_api_test/smapi/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import HelloWorld
+from drf_firebase_auth.models import FirebaseUser, FirebaseUserProvider
 
 # Register your models here.
 admin.site.register(HelloWorld)
+admin.site.register(FirebaseUser)
+admin.site.register(FirebaseUserProvider)
