c6aede3f479395166e9e248f9170b9187522f64a
kjb4494 <rlagnltn1357@gmail.com>
2019-05-29 15:48:32 +0900

    session id 사용자 인증에서도 적용되도록 코드 추가

--- firebase_api_test/smapi/views.py
+++ firebase_api_test/smapi/views.py
@@ -8,9 +8,11 @@ import hashlib
 
 # Create your views here.
 class SmbotTextQuery(views.APIView):
-    @requires_params(['query_text', 'test'])
+    @requires_params(['query_text'])
     def get(self, request):
-        session_id = hashlib.md5(request.META.get('HTTP_AUTHORIZATION').encode('utf-8')).hexdigest()
+        session_id = request.COOKIES.get('sessionid')
+        if session_id is None:
+            session_id = hashlib.md5(request.META.get('HTTP_AUTHORIZATION').encode('utf-8')).hexdigest()
         res = detect_intent_texts(session_id, request.GET.get('query_text'))
         results = serializers.DialogflowResponse(res, many=False).data
         return Response(results)
